7618164700c47400a5c88358db574290efac8964
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2022-09-24 18:14:06 +0300

    Спринт 6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -138,6 +138,6 @@ def profile_follow(request, username):
 
 @login_required
 def profile_unfollow(request, username):
-    author=get_object_or_404(User, username=username)
+    author = get_object_or_404(User, username=username)
     Follow.objects.filter(user=request.user, author=author).delete()
     return redirect("posts:profile", username=username)
